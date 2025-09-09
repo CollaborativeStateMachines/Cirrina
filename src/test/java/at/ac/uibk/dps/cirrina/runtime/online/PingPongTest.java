b657c2181da7f4fdbe7a70412d599dd0b64d38cc
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2025-09-09 21:27:36 +0200

    chore: removing all zookeeper stuff

--- src/test/java/at/ac/uibk/dps/cirrina/runtime/online/PingPongTest.java
+++ src/test/java/at/ac/uibk/dps/cirrina/runtime/online/PingPongTest.java
@@ -49,8 +49,7 @@ public class PingPongTest {
             "runtime",
             eventHandler,
             persistentContext,
-            openTelemetry,
-            false
+            openTelemetry
           );
           //runtime.run();
         }
