e09585eb1048718485a2d4b2fc98b39558c04d00
Marlon Etheredge <80909536+Frnd-me@users.noreply.github.com>
2026-01-27 12:30:13 +0100

    feat: rewriting utility functions (#79)

---
+++ src/main/java/at/ac/uibk/dps/cirrina/utils/Utils.kt
@@ -0,0 +1,8 @@
+package at.ac.uibk.dps.cirrina.utils
+
+import java.util.UUID
+import kotlin.random.Random
+
+fun getBuildVersion(): String? = object {}.javaClass.getPackage().implementationVersion
+
+fun getInsecureUuid(): UUID = UUID(Random.nextLong(), Random.nextLong())
