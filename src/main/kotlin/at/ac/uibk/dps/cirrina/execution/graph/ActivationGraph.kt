32074daa805117812daec03af470516954f587ea
Marlon Etheredge <marlon.etheredge@uibk.ac.at>
2026-02-11 17:45:39 +0100

    test: adding test

--- src/main/kotlin/at/ac/uibk/dps/cirrina/execution/graph/ActivationGraph.kt
+++ src/main/kotlin/at/ac/uibk/dps/cirrina/execution/graph/ActivationGraph.kt
@@ -1,49 +1,57 @@
 package at.ac.uibk.dps.cirrina.execution.graph
 
 import at.ac.uibk.dps.cirrina.csm.Csml.EventChannel
-import at.ac.uibk.dps.cirrina.execution.`object`.Event
 import at.ac.uibk.dps.cirrina.execution.`object`.StateMachine
 import org.jgrapht.graph.DirectedMultigraph
 
 class ActivationGraph :
   DirectedMultigraph<String, ActivationGraph.Activation>(Activation::class.java) {
-
-  data class Activation(val source: String, val target: String, val event: Event)
+  data class Activation(
+    val source: String,
+    val target: String,
+    val channel: EventChannel,
+    val topic: String,
+  )
 
   companion object {
     fun create(stateMachines: Collection<StateMachine>): ActivationGraph {
-      val graph = ActivationGraph()
+      return ActivationGraph().apply {
+        stateMachines.forEach { addVertex(it.name) }
 
-      stateMachines.forEach { graph.addVertex(it.name) }
+        for (source in stateMachines) {
+          val outputs = source.specification.outputEvents
 
-      for (source in stateMachines) {
-        val outputs = source.specification.outputEvents
+          for (event in outputs) {
+            for (target in stateMachines) {
+              if (source.name == target.name) continue
 
-        for (event in outputs) {
-          for (target in stateMachines) {
-            if (source.name == target.name) continue
+              // TODO: This cannot be matched at this point because a raise action has an
+              // expression
+              //  as target, unless we find a way to evaluate it here we cannot filter out
+              // targeted
+              //  events here
+              // if (event.target.isNotEmpty() && event.target != target.name) continue
 
-            // TODO: This cannot be matched at this point because a raise action has an expression
-            //  as target, unless we find a way to evaluate it here we cannot filter out targeted
-            //  events here
-            // if (event.target.isNotEmpty() && event.target != target.name) continue
+              if (!target.specification.inputEvents.contains(event.topic)) continue
 
-            if (!target.specification.inputEvents.contains(event.topic)) continue
+              val canDeliver =
+                when (event.channel) {
+                  EventChannel.GLOBAL -> true
+                  EventChannel.EXTERNAL -> target.isSubscribedTo(source.name)
+                  else -> false
+                }
 
-            val canDeliver =
-              when (event.channel) {
-                EventChannel.GLOBAL -> true
-                EventChannel.EXTERNAL -> target.isSubscribedTo(source.name)
-                else -> false
+              if (canDeliver) {
+                addEdge(
+                  source.name,
+                  target.name,
+                  Activation(source.name, target.name, event.channel, event.topic),
+                )
               }
-
-            if (canDeliver) {
-              graph.addEdge(source.name, target.name, Activation(source.name, target.name, event))
             }
           }
         }
       }
-      return graph
     }
   }
 }
--- src/test/kotlin/at/ac/uibk/dps/cirrina/execution/graph/ActivationGraphTest.kt
+++ src/test/kotlin/at/ac/uibk/dps/cirrina/execution/graph/ActivationGraphTest.kt
@@ -1,12 +1,12 @@
 package at.ac.uibk.dps.cirrina.execution.graph
 
+import at.ac.uibk.dps.cirrina.csm.Csml.EventChannel
 import at.ac.uibk.dps.cirrina.data.DefaultDescriptions
 import at.ac.uibk.dps.cirrina.di.DaggerTestComponent
 import at.ac.uibk.dps.cirrina.di.TestModule
 import at.ac.uibk.dps.cirrina.execution.`object`.EventHandler
 import at.ac.uibk.dps.cirrina.execution.provider.ContextInMemory
-import org.jgrapht.nio.DefaultAttribute
-import org.jgrapht.nio.dot.DOTExporter
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ActivationGraphTest {
@@ -21,18 +21,33 @@ class ActivationGraphTest {
         .build()
         .runtime()
 
-    val exporter = DOTExporter<String, ActivationGraph.Activation>()
+    val graph = ActivationGraph.create(runtime.instances.values)
 
-    exporter.setVertexAttributeProvider { vertex ->
-      mapOf("label" to DefaultAttribute.createAttribute(vertex))
+    graph.vertexSet().run {
+      val expected =
+        listOf(
+          "one",
+          "two",
+          "three",
+          "four",
+          "one.0@nestedStateMachine2",
+          "one.1@nestedStateMachine1",
+        )
+
+      assertTrue(containsAll(expected) && expected.containsAll(this))
     }
 
-    exporter.setEdgeAttributeProvider { edge ->
-      mapOf(
-        "label" to DefaultAttribute.createAttribute("${edge.event.topic} (${edge.event.channel})")
-      )
-    }
+    graph.edgeSet().run {
+      val expected =
+        listOf(
+          ActivationGraph.Activation("one", "two", EventChannel.GLOBAL, "e3"),
+          ActivationGraph.Activation("one", "three", EventChannel.GLOBAL, "e5"),
+          ActivationGraph.Activation("one", "four", EventChannel.GLOBAL, "e3"),
+          ActivationGraph.Activation("one", "four", EventChannel.GLOBAL, "e5"),
+          ActivationGraph.Activation("two", "one", EventChannel.EXTERNAL, "e4"),
+        )
 
-    exporter.exportGraph(ActivationGraph.create(runtime.instances.values), System.out)
+      assertTrue(containsAll(expected) && expected.containsAll(this))
+    }
   }
 }
